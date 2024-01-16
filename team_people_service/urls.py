82af1d968236afac847510cd3469e83f083dba4e
Eugene Rabenko <qwer09846@gmail.com>
2024-01-16 15:50:18 +0200

    rename service folder of project

--- team_people_service/urls.py
+++ team_people_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for service project.
+URL configuration for team_people_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
