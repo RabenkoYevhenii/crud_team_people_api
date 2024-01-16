46c3da39563d7e77f41c24e8ca546a3e8fa7ab4c
Eugene Rabenko <qwer09846@gmail.com>
2024-01-16 16:02:09 +0200

    add model Team & People

--- team_people_api/models.py
+++ team_people_api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class People(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        verbose_name_plural = "people"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
