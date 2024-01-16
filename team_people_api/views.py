fa78c7ba98423518d93e4daa7f3c5c13a7e28f7e
Eugene Rabenko <qwer09846@gmail.com>
2024-01-16 16:40:06 +0200

    add views

--- team_people_api/views.py
+++ team_people_api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from team_people_api.models import People, Team
+from team_people_api.serializers import (
+    PeopleSerializer,
+    TeamSerializer,
+    TeamListSerializer,
+    TeamDetailSerializer,
+)
+
+
+class Pagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class PeopleViewSet(viewsets.ModelViewSet):
+    queryset = People.objects.all()
+    serializer_class = PeopleSerializer
+    pagination_class = Pagination
+
+
+class TeamPeopleViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.prefetch_related("members")
+    serializer_class = TeamSerializer
+    pagination_class = Pagination
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TeamListSerializer
+
+        if self.action == "retrieve":
+            return TeamDetailSerializer
+
+        return TeamSerializer
