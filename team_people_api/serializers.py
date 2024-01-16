920fcc4bd1c0142548c9b06687203846055e95ae
Eugene Rabenko <qwer09846@gmail.com>
2024-01-16 16:24:53 +0200

    add serializers

---
+++ team_people_api/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+
+from team_people_api.models import Team, People
+
+
+class PeopleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = People
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = (
+            "id",
+            "name",
+        )
+
+
+class TeamListSerializer(TeamSerializer):
+    members = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="last_name"
+    )
+
+    class Meta:
+        model = Team
+        fields = (
+            "id",
+            "name",
+            "members",
+        )
+
+
+class TeamDetailSerializer(TeamSerializer):
+    members = PeopleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Team
+        fields = (
+            "id",
+            "name",
+            "members",
+        )
+
+
